de178ea310afef224674bd686da46b375cee220a
Lachlan Bradford <lgbradford@indeed.com>
2019-01-26 13:33:35 +0900

    add test image

--- src/components/HouseMap.jsx
+++ src/components/HouseMap.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
+import MapImage from "../assets/test_map.png";
+
 const Container = styled.div`
   width: 100vw;
   height: 100vw;
-  background-color: red;
+  background-color: turquoise;
 `;
 
-const HouseMap = () => <Container />;
+const StyledImg = styled.img`
+  width: 100%;
+`;
+
+const HouseMap = () => (
+  <Container>
+    <StyledImg src={MapImage} />
+  </Container>
+);
 
 export default HouseMap;
