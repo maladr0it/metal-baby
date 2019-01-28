09c4fa86df3920bb4d1c441e7636df6cb47c9e69
Lachlan Bradford <lgbradford@indeed.com>
2019-01-28 17:33:32 +0900

    reduced acceleration

--- src/gameConfig.js
+++ src/gameConfig.js
@@ -1,9 +1,9 @@
 // How many ms per game-tick
 // lower is faster :S
-export const initialSpeed = 2;
-export const ticksPerSpeedup = 2;
-export const speedUpPercent = 10;
-export const maxSpeed = 0.7;
+export const initialSpeed = 2.5;
+export const ticksPerSpeedup = 5;
+export const speedUpPercent = 15;
+export const maxSpeed = 1;
 export const ticksPerYear = 5;
 
 export const maxNeeds = {
@@ -26,23 +26,23 @@ export const needsDecay = {
 };
 
 // How much random variance effects have on your needs
-export const effectVariance = 0.5;
+export const effectVariance = 0.3;
 
 // The effects of each activity
 export const eatEffect = {
   hunger: +30,
-  hygiene: -8,
+  hygiene: -10,
   fun: 0
 };
 
 export const batheEffect = {
   hunger: 0,
   hygiene: +30,
-  fun: -8
+  fun: -10
 };
 
 export const playEffect = {
-  hunger: -8,
+  hunger: -10,
   hygiene: 0,
   fun: +30
 };
